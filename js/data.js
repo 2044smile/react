fb98e1a2ec6c4a664598d694e65faffb45e34e70
2044smile <2044smile@naver.com>
2024-10-13 13:08:57 +0900

    feat: learned data and operators

---
+++ js/data.js
@@ -0,0 +1,38 @@
+// ECMAScript -> e.g) ES6
+// 프로그램이 실행되는 run 타임에 코드가 실행된다.
+
+// 자료형
+// number type
+let n1 = 1234;
+let n2 = 5.66;
+
+// string type
+let s1 = "Hello";
+let s2 = 'world';
+
+// bool type
+let b1 = true;
+let b2 = false;
+
+// null type
+let n = null;
+
+// undefined type
+let u1;
+let u2 = undefined;
+
+// array type
+let arr1 = [1,2,3,4,5];
+let arr2 = ['h','e','l']
+let arr3 = ['a', 1]
+let arr4 = [true, 1, undefined, false, 'h', 2, null]
+
+// object type
+let obj1 = {a: 'apple', b: "banana", c: "carrot"}
+let obj2 = {a:1, b:2, c: [1,2,3,4]} 
+let obj3 = {a: "hello", b: 100, c: [1,2,3,4]}
+let obj4 = {
+    a: {a1: 1, a2: 2},
+    b: {b1: 1, b2: 2},
+    c: {c1: 1, c2: 2},
+}
---
+++ js/operator.js
@@ -0,0 +1,60 @@
+// 연산자
+// 대입 연산자 assignment operator
+let a = 10;
+let b = 20;
+
+// 사칙 연산자 +, -, *, /, %, 지수 연산자(**)
+
+a += b;
+a -= b;
+a *= b;
+a /= b;
+
+// 증가 연산자 ++ 감소 연산자 --
+// postfix 방식: a++
+// prefix 방식: ++a
+let a = 1;
+let b = a++;
+console.log(a,b);
+// 출력 결과: 2, 1
+
+let a = 1;
+let b = ++a;
+console.log(a,b);
+// 출력 결과: 2, 2
+
+// 관계 연산자 (Relational operators)
+// <, >, <=, >=
+let a = 1;
+let b = 2;
+
+console.log(a < b);
+// 출력 결과: true
+
+// 동등 연산자 (Equality operators)
+// a == b, a != b 자료형은 신경쓰지 않는다
+
+// 일치 연산자 (Strict(엄격한) equality operators)
+// a === b, a !== b 자료형이 같은지 확인
+
+let a = 1;
+let b = '1';
+
+console.log(a == b); 
+// 출력 결과: ture
+console.log(a === b);
+// 출력 결과: false
+
+// 이진 논리 연산자 (Binary logical operators)
+// a && b (and), a || b (or)
+
+// 조건부 연산자 == 삼항 연산자 (Conditional operators == Ternary operator)
+// 조건식 ? true 일 경우 : false 일 경우
+let a = true;
+let b = false;
+
+console.log(a ? 1 : 2);
+// 출력 결과: 1
+console.log(b ? 1 : 2);
+// 출력 결과: 2
+// 1 = true, 2 = false
