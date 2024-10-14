20b9a11c7f476a3fca6731f0c9a7dd10b3315295
2044smile <2044smile@naver.com>
2024-10-14 18:53:13 +0900

    feat: function

---
+++ js/function.js
@@ -0,0 +1,18 @@
+// 함수란 입력을 받아서 정해진 출력을 하는 것
+// 파라미터(parameter), 인자(argument)
+
+// function statement 사용
+function sum(a, b) {
+    return a + b;
+}
+
+console.log(sum(10, 20))
+// 출력 결과: 30
+
+// arrow function expression 사용
+const multiply = (a, b) => {
+    return a * b;
+}
+
+console.log(multiply(10, 20))
+// 출력 결과: 200
