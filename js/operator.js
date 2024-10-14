8aa0c708bbdb4657880cf01d6778b824f6408204
2044smile <2044smile@naver.com>
2024-10-14 19:33:52 +0900

    fix: minor

--- js/operator.js
+++ js/operator.js
@@ -13,22 +13,22 @@ a /= b;
 // 증가 연산자 ++ 감소 연산자 --
 // postfix 방식: a++
 // prefix 방식: ++a
-let a = 1;
-let b = a++;
-console.log(a,b);
+let c = 1;
+let d= c++;
+console.log(c,d);
 // 출력 결과: 2, 1
 
-let a = 1;
-let b = ++a;
-console.log(a,b);
+let e = 1;
+let f = ++e;
+console.log(e,f);
 // 출력 결과: 2, 2
 
 // 관계 연산자 (Relational operators)
 // <, >, <=, >=
-let a = 1;
-let b = 2;
+let g = 1;
+let h = 2;
 
-console.log(a < b);
+console.log(g < h);
 // 출력 결과: true
 
 // 동등 연산자 (Equality operators)
@@ -37,12 +37,12 @@ console.log(a < b);
 // 일치 연산자 (Strict(엄격한) equality operators)
 // a === b, a !== b 자료형이 같은지 확인
 
-let a = 1;
-let b = '1';
+let i = 1;
+let j = '1';
 
-console.log(a == b); 
+console.log(i == j); 
 // 출력 결과: ture
-console.log(a === b);
+console.log(i === j);
 // 출력 결과: false
 
 // 이진 논리 연산자 (Binary logical operators)
@@ -50,11 +50,11 @@ console.log(a === b);
 
 // 조건부 연산자 == 삼항 연산자 (Conditional operators == Ternary operator)
 // 조건식 ? true 일 경우 : false 일 경우
-let a = true;
-let b = false;
+let k = true;
+let l = false;
 
-console.log(a ? 1 : 2);
+console.log(k ? 1 : 2);
 // 출력 결과: 1
-console.log(b ? 1 : 2);
+console.log(l ? 1 : 2);
 // 출력 결과: 2
 // 1 = true, 2 = false
