02648d7fe0c0ad4838bacca24adc3dfa4a4693cb
cs_lee <2044smile@naver.com>
2024-10-17 22:51:34 +0900

    fix: minor

--- todolist/src/App.js
+++ todolist/src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+
+// 컴포넌트는 React의 props를 통해 데이터를 받을 수 있고, JSX를 반환해서 화면에 UI를 그려주는 역할
+// props는 React 컴포넌트 간에 데이터를 전달할 때 사용하는 객체
+// const 로 이 컴포넌트를 선언한 이유는 컴포넌트 자체가 재할당되지 않도록 하기 위해서
+const TodoItemInputField = (props) => {
+  const [input, setInput] = useState(""); // React의 함수형 컴포넌트 내부에서 상태(state)를 관리하기 위해 변수를 선언
+  // React의 useState 훅을 이용해서 컴포넌트 내에서 상태를 선언
+
+  const onSubmit = () => {
+    props.onSubmit(input);
+    setInput("");
+  };
+
+  return (<div>
+    <TextField
+      id="todo-item-input"
+      label="Todo Item"
+      variant="outlined"
+      onChange={(e) => setInput(e.target.value)} value={input}  // input의 값을 업데이트한다.
+    />
+    <Button variant="outlined" onClick={onSubmit}>Submit</Button>
+  </div>)
+};
+
+const TodoItemList = (props) => {
+  return (<div>
+
+  </div>);
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <TodoItemInputField onSubmit={() => {}} />
+      <TodoItemList />
     </div>
   );
 }
--- todolist/src/index.js
+++ todolist/src/index.js
@@ -1,22 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-// import App from './App';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
 // import { BrowserRouter } from "react-router-dom";
-
-import Create from './components/create';
-import Retrieve  from './components/retrieve';
-import Update from './components/update';
-import Delete from './components/delete';
+// index.js 는 보통 전체 애플리케이션의 진입점 역할
+// 즉, React 애플리케이션이 실행되기 시작하는 파일이 index.js 이기 때문에 
+// CRUD 기능을 여기 직접 넣기보다는 index.js 에서 최상위 컴포넌트 App.js 를 렌더링하는 방식으로 구현하는 것이 좋다.
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
-    <Create />
-    <Retrieve />
-    <Update />
-    <Delete />
+    <App />
   </React.StrictMode>
 );
 
